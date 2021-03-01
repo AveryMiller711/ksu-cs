3dc55f4a00fc681707f3409c94dac9dd9e34f40c
Avery Miller <ajmiller2060@yahoo.com>
2021-03-01 14:41:41 -0500

    stack ADT

---
+++ CS2/stack/stack.hpp
@@ -0,0 +1,29 @@
+// stack.hpp
+// Avery Miller
+//
+// 03/01/2021
+
+#ifndef CS2_STACK_HPP
+#define CS2_STACK_HPP
+
+#include <cassert>
+
+const int SIZE = 100;
+
+template <typename T>
+class stack {
+public:
+    stack() : tos(-1) {};
+    bool isEmpty() const { return tos == -1; };
+    bool isFull() const { return tos >= SIZE-1; };
+    void push(const T& item) { assert(!isFull()); s[++tos] = item; };
+    T pop() { assert(!isEmpty()); return s[tos--]; };
+    T top() const { assert(!isEmpty()); return s[tos]; };
+
+private:
+    int tos;
+    T s[SIZE];
+};
+
+
+#endif
